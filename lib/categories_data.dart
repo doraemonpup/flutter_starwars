ff01e9684bb2cc6dd431dc00e05432c2a563aeb8
Chunhawit <pupz28@gmail.com>
2021-08-29 20:24:40 +0200

    remove comments

--- lib/categories_data.dart
+++ lib/categories_data.dart
@@ -1,26 +1,3 @@
-// import './models/category.dart';
-
-// const CATEGORIES = const [
-//   Category(
-//     title: 'people',
-//   ),
-//   Category(
-//     title: 'planets',
-//   ),
-//   Category(
-//     title: 'films',
-//   ),
-//   Category(
-//     title: 'species',
-//   ),
-//   Category(
-//     title: 'vehicles',
-//   ),
-//   Category(
-//     title: 'starships',
-//   ),
-// ];
-
 const CATEGORIES = const [
   'people',
   'planets',
